66ed51752cb53c62d9617dff6dba17d404f7e65c
Stephen Perrin <Sperrin7009@conestogac.on.ca>
2019-11-18 08:42:48 -0400

    Added a Main DOxygen page that holds Project Title, Authors, Version, Project Requirements, and Project Description (Project Description is incomplete - Gonna work on it after we got more stuff)

---
+++ TTT_TMSv2/DOxygenMainProjectPage.cs
@@ -0,0 +1,25 @@
+﻿/// \mainpage TTT TMS Project   
+///
+/// \section intro Program Introduction
+/// The <b>Transport Management System(TMS)</b> project application is meant to 
+///
+/// <hr>
+/// \section requirements Project Requirements
+/// <blockquote>
+/// \htmlonly
+/// <embed src = "SQ-TermProjectOverview-TMSv2.pdf" width = "100%" heigth = "100%" href = "SQ-TermProjectOverview-TMSv2.pdf"></embed>
+/// \endhtmlonly
+/// </blockquote>
+///
+/// <hr>
+/// \section version Current version of the TMS Project :
+/// <ul>
+/// <li>\authors   Joel Malleck, Stephen Perrin, Kieran Campbell</li>
+/// <li>\version   1.00.00</li>
+/// <li>\date      2019-11-18</li>
+/// <li>\pre       <b>N/A</b></li>
+/// <li>\warning   <b>N/A</b></li>
+/// <li>\copyright <b>N/A</b></li>
+/// </ul>
+///
+
