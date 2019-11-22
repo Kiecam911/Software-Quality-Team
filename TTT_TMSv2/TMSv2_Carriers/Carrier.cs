df4bca3055d1bd8370705c3e6a0f5f1b4902d84b
Kieran Campbell <Kcampbell7760@conestogac.on.ca>
2019-11-22 14:59:41 -0500

    Added data members to carrier class, and set up test method for carrier class

--- TTT_TMSv2/TMSv2_Carriers/Carrier.cs
+++ TTT_TMSv2/TMSv2_Carriers/Carrier.cs
@@ -8,5 +8,80 @@ namespace TMSv2_Carriers
 {
     public class Carrier
     {
+        private int carrierID;
+        private string carrierName;
+        private bool isFTL;
+        private float perPalletLTLRate;
+        private bool isReefer;
+        private float carrierCapacity;
+
+        public void setCarrierID(int newID)
+        {
+            carrierID = newID;
+        }
+
+        public void setCarrierName(string newName)
+        {
+            if (newName != "")
+            {
+                carrierName = newName;
+            }
+        }
+
+        public void setIsFTL(bool state)
+        {
+            isFTL = state;
+        }
+
+        public void setPerPalletLTLRate(float newRate)
+        {
+            if (newRate >= 0)
+            {
+                perPalletLTLRate = newRate;
+            }
+        }
+
+        public void setIsReefer(bool state)
+        {
+            isReefer = state;
+        }
+
+        public void setCarrierCapacity(float newCap)
+        {
+            if (newCap >= 0)
+            {
+                carrierCapacity = newCap;
+            }
+        }
+
+        public int getCarrierID()
+        {
+            return carrierID;
+        }
+
+        public string getCarrierName()
+        {
+            return carrierName; 
+        }
+
+        public bool getIsFTL()
+        {
+            return isFTL;
+        }
+
+        public float getPerPalletFTLRate()
+        {
+            return perPalletLTLRate;
+        }
+
+        public bool getIsReefer()
+        {
+            return isReefer;
+        }
+
+        public float getCarrierCapacity()
+        {
+            return carrierCapacity;
+        }
     }
 }
