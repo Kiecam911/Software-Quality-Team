beee46ea3273916cea8296dd789e367af24692be
Kieran Campbell <Kcampbell7760@conestogac.on.ca>
2019-12-05 03:02:42 -0500

    Created menu screen, and started Admin screen

---
+++ TTT_TMSv2/TMSv2_UIClass/Pages/MenuPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TMSv2_UIClass.Pages
+{
+    /// <summary>
+    /// Interaction logic for MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void adminButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Find the frame.
+            Frame frame = null;
+            DependencyObject parent = VisualTreeHelper.GetParent(this);
+
+            // Cycles through to MainWindow frame
+            while (parent != null && frame == null)
+            {
+                frame = parent as Frame;
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            // Change the page of the frame.
+            if (frame != null)
+            {
+                frame.Navigate(new AdminPage());
+            }
+        }
+    }
+}
