e6d9a5d14860adeb0058f5cca05c2761c84d3be2
Stephen Perrin <Sperrin7009@conestogac.on.ca>
2019-12-05 13:42:44 -0500

    Added DOxygen comments to the code

--- TTT_TMSv2/DAL/DataAccess.cs
+++ TTT_TMSv2/DAL/DataAccess.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
-
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using System.Configuration;
@@ -14,8 +13,30 @@ using System.Configuration;
 
 namespace TMSv2_DAL
 {
+    /// 
+    /// \class DataAccess
+    ///
+    /// \brief The purpose of this class is to model a contract within the system.
+    /// \details <b>Details</b>
+    ///
+    /// This class contains the methods to connect to the databases that are used in this project
+    /// 
+    ///
+    /// \author <i>TeamTeamTeam</i>
+    /// 
+    ///
     public static class DataAccess
     {
+        ///
+        /// \brief To retrieve the contracts from the Contract Marketplace
+        /// \details <b>Details</b>
+        ///
+        /// This method interfaces with the contract marketplace database and returns the dataset
+        ///
+        /// \param <b>void</b> - None
+        ///
+        /// \return DataSet Returns the contracts in the contract marketplace
+        ///
         public static DataSet GetContracts()
         {
             // initialize query and connection string
